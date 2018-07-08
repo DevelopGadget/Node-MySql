37d63f7b42c77eab287c7e678c03fc564d51264e
FernanPro18 <fernandoaraujo081@gmail.com>
2018-07-08 09:30:03 -0500

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict'
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const body = require('body-parser');
+
+//Puerto
+app.set('port', process.env.PORT || 3000);
+
+//Uses
+app.use(morgan('dev'));
+app.use(body.json());
+app.use(body.urlencoded({ extended: false }));
+
+require('./Routes/Routes')(app);
+
+//Listen
+app.listen(app.get('port'), () => {
+  console.log('Iniciado ' + app.get('port'));
+});
